5b05776c9e4f6a0048ebe80324746b989f49d530
JCamarenaUABC <jcamarena@uabc.edu.mx>
2020-12-27 08:54:05 -0800

    CONF DB

--- config/settings.py
+++ config/settings.py
@@ -89,18 +89,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-"""
+
 DATABASES = {
     "default": env.dj_db_url("DATABASE_URL")
 }
-"""
 
-DATABASES = {
+
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
 
 # Password validation
